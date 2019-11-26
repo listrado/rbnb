57dfb41fdcb0916b5802241f4b7708f14f89dac2
arigbede emmanuel <Harriemannie@gmail.com>
2019-11-26 16:48:15 -0300

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating Users and ArmoredCars'
+
+User.create(email: 'user1@gmail.com',
+            password: '123456',
+            name: 'Andre')
+
+User.create(email: 'user2@gmail.com',
+            password: '123456',
+            name: 'Monster')
+
+ArmoredCar.create(model: 'Tanque Argentino Mediano',
+                  detailes: 'medium tank',
+                  price_cents: 2_000,
+                  user_id: 1)
+
+ArmoredCar.create(model: 'EE-3 Jararaca',
+                  detailes: ' Brazilian scout car ',
+                  price_cents: 1_000,
+                  user_id: 1)
+
+ArmoredCar.create(model: 'EE-9 Cascavel',
+                  detailes: ' a six-wheeled Brazilian armoured car developed primarily for reconnaissance',
+                  price_cents: 2_000,
+                  user_id: 1)
+
+ArmoredCar.create(model: 'VBTP-MR Guarani',
+                  detailes: 'Medium Wheeled Type',
+                  price_cents: 2_000,
+                  user_id: 1)
+
+ArmoredCar.create(model: 'Batmobile',
+                  detailes: "Andre's favorite",
+                  price_cents: 9_000,
+                  user_id: 1)
+
+puts 'Seed complete'
