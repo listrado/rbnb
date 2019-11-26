f9ac44133521a6a87198d3f51ceca745e43d1c46
lucas cardi <llucas.cardi.rangel@gmail.com>
2019-11-26 19:03:42 -0300

    new create delete for car

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources   :users,        only: %i[show]
-  resources   :armored_cars, only: %i[edit update] do
+  resources   :armored_cars, only: %i[edit update new create delete] do
     resources :bookings,     only: %i[show delete create new]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
