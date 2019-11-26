32a0b665e269a4a6d5d93397d91454dc2e1b8f2e
lucas cardi <llucas.cardi.rangel@gmail.com>
2019-11-26 14:22:29 -0300

    generate booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_171851) do
+ActiveRecord::Schema.define(version: 2019_11_26_172158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "armored_cars", force: :cascade do |t|
+    t.string "model"
+    t.string "detailes"
+    t.integer "price_cents"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_armored_cars_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "rent_price_cents"
+    t.date "starts_on"
+    t.date "ends_on"
+    t.bigint "user_id"
+    t.bigint "armored_car_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["armored_car_id"], name: "index_bookings_on_armored_car_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +50,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_171851) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "armored_cars", "users"
+  add_foreign_key "bookings", "armored_cars"
+  add_foreign_key "bookings", "users"
 end
