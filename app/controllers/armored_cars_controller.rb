186cd7f44b36a870911584aded3832eb1ca4748b
arigbede emmanuel <Harriemannie@gmail.com>
2019-11-26 19:15:16 -0300

    add new, create and destroy to armored cars

--- app/controllers/armored_cars_controller.rb
+++ app/controllers/armored_cars_controller.rb
@@ -1,4 +1,19 @@
 class ArmoredCarsController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    @armored_car = ArmoredCar.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @armored_car = ArmoredCar.new
+    @armored_car.user = @user
+    @armored_car.save
+
+    # redirect_to garden_path(@garden)
+    redirect_to @user
+  end
+
   def edit
     @car = ArmoredCar.find(params[:id])
   end
@@ -7,6 +22,15 @@ class ArmoredCarsController < ApplicationController
     @car = ArmoredCar.find(params[:id])
     @car.update(car_params)
     redirect_to '/'
+  end
+
+  def destroy
+    @armored_car = Armored_car.find(params[:id])
+    @armored_car.destroy
+
+    redirect_to @armored_car.user
+  end
+
   end
 
   private
