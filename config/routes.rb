a8452832d13af78fc960664aedcd838f6debfdf4
lucas cardi <llucas.cardi.rangel@gmail.com>
2019-11-26 15:37:28 -0300

    add routes cars/users/bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources   :users,        only: %i[show]
+  resources   :armored_cars, only: %i[edit update] do
+    resources :bookings,     only: %i[show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
