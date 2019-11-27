e35303494d543d53b788496909cae0c9d719886c
lucas cardi <llucas.cardi.rangel@gmail.com>
2019-11-27 13:11:25 -0300

    add all routes to ac && bookinhgs

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources   :users,        only: %i[show]
-  resources   :armored_cars, only: %i[edit update new create delete show] do
-    resources :bookings,     only: %i[show delete create new]
+  resources   :armored_cars do
+    resources :bookings
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
