efe58472601ccbc20e8557832cc23735cd3d4aa0
lucas cardi <llucas.cardi.rangel@gmail.com>
2019-11-28 16:40:55 -0300

    fixed calculations on price glitch

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,9 +7,7 @@ class BookingsController < ApplicationController
   def create
     @armored_car = ArmoredCar.find(params[:armored_car_id])
     @booking = Booking.new(booking_params)
-    @days = (@booking.ends_on - @booking.starts_on).to_i
-    @total = @days * @armored_car.price_cents.to_i
-    @booking.rent_price_cents = @total
+    @booking.rent_price_cents = @armored_car.price_cents
     @booking.user = current_user
     @booking.armored_car = @armored_car
     if @booking.save
