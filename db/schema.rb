f363e4c9ba1a4c6f0fb743ab68111f626604fe52
arigbede emmanuel <Harriemannie@gmail.com>
2019-11-29 12:40:05 -0300

    fix-seed-address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_181256) do
+ActiveRecord::Schema.define(version: 2019_11_29_144418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 2019_11_28_181256) do
     t.bigint "armored_car_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
     t.index ["armored_car_id"], name: "index_bookings_on_armored_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
