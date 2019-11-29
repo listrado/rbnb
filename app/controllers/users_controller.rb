086fcc9cd879444c8133438af6d1a12b04cad2ea
arigbede emmanuel <Harriemannie@gmail.com>
2019-11-29 16:03:33 -0300

    round check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @bookings = current_user.bookings
+    @cars = ArmoredCar.all
+    @own_cars = @cars.select{ |car| car.user == current_user }
   end
 end
